4997792edc806314f3984bb131d71a86d2f0493e
mjk729 <kimminjung0729@naver.com>
2023-08-02 19:12:50 +0900

    [김민중] - sts 환경 설정

---
+++ src/main/java/com/memo/pk/MemoProjectApplication.java
@@ -0,0 +1,16 @@
+package com.memo.pk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication
+public class MemoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/memo/pk/MemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.memo.pk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
